e76d4c7a7535a4b37e21f1bbb7587c9a29a716c2
bernardofreire <b.bernardo9815@gmail.com>
2022-06-21 01:50:05 -0300

    mudança no layout

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+const header = document.getElementById('header')
+header.addEventListener('mousemove', parallax)
+
+function parallax(e) {
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX * speed) / 100
+        const y = (window.innerHeight - e.pageY * speed) / 100
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px) `
+
+    })
+}
+
+
